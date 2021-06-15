7e7778946917e873c502d340efadd1f0ea4f243a
Justinas Grigas <jstn_as@protonmail.com>
2021-06-15 13:26:54 +0100

    st replacer now edits config.def.h

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def load_palette(palette_filepath: str):
 
 if __name__ == '__main__':
     palette = load_palette('palettes/fellow.txt')
-    SlockReplacer('/home/iota/repos/slock/config.def.h', palette)
-    StReplacer('/home/iota/repos/st/palette.c', palette)
-    DwmReplacer('/home/iota/repos/dwm/palette.c', palette)
-    DwmReplacer('/home/iota/repos/dmenu/palette.c', palette)
-    TabbedReplacer('/home/iota/repos/tabbed/palette.c', palette)
-    MtReplacer('monkeytype.txt', palette)
+#    SlockReplacer('/home/iota/repos/slock/config.def.h', palette)
+    StReplacer('/home/iota/repos/st/config.def.h', palette)
+#    DwmReplacer('/home/iota/repos/dwm/palette.c', palette)
+#    DwmReplacer('/home/iota/repos/dmenu/palette.c', palette)
+#    TabbedReplacer('/home/iota/repos/tabbed/palette.c', palette)
+#    MtReplacer('monkeytype.txt', palette)
 
 
